1d4c314791beb429b1c2587cec3b0ee7f7a5625e
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-03-31 10:49:59 +0300

    Update Balloons.h

--- week5/Balloons.h
+++ week5/Balloons.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include "GlobalConstants.h"
+using namespace GlobalConstants;
 
 struct Balloons {
 private:
@@ -11,10 +12,47 @@ private:
         PINK
     };
 
-    BalloonColor getColorFromStr (const char* str);
-    double balloonPrice = 0.0;
-    //int balloonsFound [GlobalConstants::NAME_MAX_SIZE] = {0};
-//po dobre shte e s bitset
+    BalloonColor color = BalloonColor::GREEN;
+    double price = 0.0;
+    int balloonsFound [GlobalConstants::NAME_MAX_SIZE] = {0};
 
+    BalloonColor getColorFromStr (const char* str) {
+        if(!str) {
+            return {};
+        }
+
+        if (strcmp("green", str) == 0) {
+            return color = BalloonColor::GREEN;
+        }
+
+        if (strcmp("red", str) == 0) {
+            return color = BalloonColor::RED;
+        }
+
+        if (strcmp("yellow", str) == 0) {
+            return color = BalloonColor::YELLOW;
+        }
+
+        if (strcmp("pink", str) == 0) {
+            return color = BalloonColor::PINK;
+        }
+    }
+
+public:
     Balloons() = default;
+
+    void setBalloonColor (const char* colorName) {
+        if (!colorName) {
+            return;
+        }
+
+        getColorFromStr(colorName);
+    }
+    void setBalloonPrice (double newPrice) {
+        if (newPrice < EPS || std::abs(newPrice - price) < EPS) {
+            return;
+        }
+
+        price = newPrice;
+    }
 };
