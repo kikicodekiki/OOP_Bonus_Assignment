8950e9d3496cbe975c561c2dabd666bc66e0a97f
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-03-31 16:28:01 +0300

    Update Client.h

--- week5/Client.h
+++ week5/Client.h
@@ -1,1 +1,20 @@
 #pragma once
+#include <iostream>
+
+using GlobalConstants::NAME_MAX_SIZE;
+
+struct Client {
+private:
+    char* clientName;
+    char fileName[NAME_MAX_SIZE];
+    //dinamichna pamet -> big 4
+
+    void copyFrom(const Client& data);
+    void free();
+
+public:
+    Client();
+    ~Client();
+    Client (const Client& other);
+    Client& operator=(const Client& other);
+};
