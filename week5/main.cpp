badd69ed648c1ab32fa1a3db9c6cada3a775ed1b
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-03-31 18:27:31 +0300

    Update main.cpp

--- week5/main.cpp
+++ week5/main.cpp
@@ -5,7 +5,7 @@
 
 using namespace GlobalConstants;
 
-double calculateDailyProfit(Client clients[], int numberOfClients, NightClub& club, const char* fileName) {
+double calculateDailyProfitWithFile(Client clients[], int numberOfClients, NightClub& club, const char* fileName) {
     std::ofstream ofs (fileName, std::ios::out|std::ios::binary);
 
     double dailyProfit = 0.0;
@@ -15,7 +15,7 @@ double calculateDailyProfit(Client clients[], int numberOfClients, NightClub& cl
             int size = strlen(clients[i].getClientName()) + 1;
             ofs.write(clients[i].getClientName(), size );
 
-            double profit = club.returnBill(clients[i].getFileName());
+            double profit = club.returnBillFromFile(clients[i].getFileName());
             ofs.write((const char*)&profit, sizeof(double));
 
             dailyProfit += profit;
@@ -30,9 +30,34 @@ double calculateDailyProfit(Client clients[], int numberOfClients, NightClub& cl
 
 }
 
+double calculateDailyProfit (Client clients[], int numberOfClients, NightClub& club) {
+    double dailyProfit = 0.0;
+
+    for (int i = 0; i < numberOfClients; ++i) {
+
+        double profit = club.returnBill();
+
+        dailyProfit += profit;
+    }
+    return dailyProfit;
+}
+
 int main() {
 
     NightClub club;
 
+    //adding inventory
+    club.addAlcohol("black_label", 2.50);
+    club.addBalloon("green", 10.30);
+    club.addShisha("OS-queen", 2.80);
+
+    // проверка на наличност
+    std::cout << "Has green balloon: " << club.hasBalloon("green") << std::endl;
+    std::cout << "Has black label alcohol: " << club.hasAlcohol("black_label") << std::endl;
+    std::cout << "Has OS queen shisha: " << club.hasNargile("OS-queen") << std::endl;
+
+    Client clients[1] = { {"John", "john.txt"} };
+    std::cout << "The bill is: ";
+    std::cout << calculateDailyProfit(clients, 1, club);
 
 }
