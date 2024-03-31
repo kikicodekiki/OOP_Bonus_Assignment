3768355603487a637e4c6d94033b71f6552c7370
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-03-31 18:03:33 +0300

    Update main.cpp

--- week5/main.cpp
+++ week5/main.cpp
@@ -1,12 +1,42 @@
+#pragma once
 #include <iostream>
 #include "Narga.h"
 #include "Baloons.h"
 #include "Alchohol.h"
 #include "GlobalConstants.h"
+#include "NightClub.h"
+#include "Client.h"
 
 using namespace GlobalConstants;
 
+double calculateDailyProfit(Client clients[], int numberOfClients, NightClub& club, const char* fileName) {
+    std::ofstream ofs (fileName, std::ios::out|std::ios::binary);
+
+    double dailyProfit = 0.0;
+
+    if (ofs.is_open()) {
+        for (int i = 0; i < numberOfClients; ++i) {
+            int size = strlen(clients[i].getClientName()) + 1;
+            ofs.write(clients[i].getClientName(), size );
+
+            double profit = club.returnBill(clients[i].getFileName());
+            ofs.write((const char*)&profit, sizeof(double));
+
+            dailyProfit += profit;
+
+        }
+
+        ofs.write((const char*)&dailyProfit, sizeof (double));
+
+    }
+
+    return dailyProfit;
+
+}
 
 int main() {
+
+    NightClub club;
+
 
 }
