7b62720dba309a210a65c90bf934e7684a244807
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-03-31 09:11:20 +0300

    Update Alchohol.h

--- week5/Alchohol.h
+++ week5/Alchohol.h
@@ -1,1 +1,58 @@
 #pragma once
+#include "GlobalConstants.h"
+
+using namespace GlobalConstants;
+
+struct Alcohol {
+private:
+    enum class AlcoholName {
+        MOET,
+        JAMESON,
+        BLACK_LABEL,
+        DON_JULIO_1942
+    };
+
+    AlcoholName name = AlcoholName::MOET;
+    double price = 0.0;
+
+    AlcoholName getColorFromStr (const char* str) {
+        if(!str) {
+            return {};
+        }
+
+        if (strcmp("black_label", str) == 0) {
+            return name = AlcoholName::BLACK_LABEL;
+        }
+
+        if (strcmp("jameson", str) == 0) {
+            return name = AlcoholName::JAMESON;
+        }
+
+        if (strcmp("moet", str) == 0) {
+            return name = AlcoholName::MOET;
+        }
+
+        if (strcmp("don_julio_1942", str) == 0) {
+            return name = AlcoholName::DON_JULIO_1942;
+        }
+    }
+
+public:
+    Alcohol() = default;
+
+    void setAlchoholName (const char* newName) {
+        if(!newName) {
+            return;
+        }
+
+        getColorFromStr(newName);
+    }
+
+    void setAlchoholPrice (double newPrice) {
+        if (newPrice < EPS || std::abs(newPrice - price) < EPS) {
+            return;
+        }
+
+        price = newPrice;
+    }
+};
