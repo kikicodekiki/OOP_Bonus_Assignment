11eb0c0d73019c380fc12f079ad7ba9401f81313
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-03-31 16:49:39 +0300

    Update GlobalConstants.h

--- week5/GlobalConstants.h
+++ week5/GlobalConstants.h
@@ -2,8 +2,9 @@
 #include <iostream>
 
 namespace GlobalConstants {
-    constexpr size_t NAME_MAX_SIZE = 50;
+    constexpr size_t NAME_MAX_SIZE = 20;
     constexpr size_t BALLOONS_MAX_NUMBER = 1024;
     constexpr size_t ALCOHOL_MAX_NUMBER = 512;
     constexpr size_t NARGAS_MAX_NUMBER = 256;
+    constexpr double EPS = 0.00000001;
 }
