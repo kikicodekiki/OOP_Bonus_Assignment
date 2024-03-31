ba98c698faf341f48f9ef4f439f56285204ed99f
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-03-31 16:46:08 +0300

    Update Balloons.h

--- week5/Balloons.h
+++ week5/Balloons.h
@@ -3,40 +3,40 @@
 #include "GlobalConstants.h"
 using namespace GlobalConstants;
 
+enum class BalloonColor {
+    GREEN,
+    RED,
+    YELLOW,
+    PINK
+};
+
+BalloonColor getColorFromStr (const char* str) {
+    if(!str) {
+        return {};
+    }
+
+    if (strcmp("green", str) == 0) {
+        return BalloonColor::GREEN;
+    }
+
+    if (strcmp("red", str) == 0) {
+        return BalloonColor::RED;
+    }
+
+    if (strcmp("yellow", str) == 0) {
+        return BalloonColor::YELLOW;
+    }
+
+    if (strcmp("pink", str) == 0) {
+        return BalloonColor::PINK;
+    }
+}
+
 struct Balloons {
 private:
-    enum class BalloonColor {
-        GREEN,
-        RED,
-        YELLOW,
-        PINK
-    };
 
     BalloonColor color = BalloonColor::GREEN;
     double price = 0.0;
-    int balloonsFound [GlobalConstants::NAME_MAX_SIZE] = {0};
-
-    BalloonColor getColorFromStr (const char* str) {
-        if(!str) {
-            return {};
-        }
-
-        if (strcmp("green", str) == 0) {
-            return color = BalloonColor::GREEN;
-        }
-
-        if (strcmp("red", str) == 0) {
-            return color = BalloonColor::RED;
-        }
-
-        if (strcmp("yellow", str) == 0) {
-            return color = BalloonColor::YELLOW;
-        }
-
-        if (strcmp("pink", str) == 0) {
-            return color = BalloonColor::PINK;
-        }
-    }
 
 public:
     Balloons() = default;
@@ -46,7 +46,7 @@ public:
             return;
         }
 
-        getColorFromStr(colorName);
+        color = getColorFromStr(colorName);
     }
     void setBalloonPrice (double newPrice) {
         if (newPrice < EPS || std::abs(newPrice - price) < EPS) {
@@ -54,5 +54,13 @@ public:
         }
 
         price = newPrice;
+    }
+
+    BalloonColor getBalloonColor () const {
+        return color;
+    }
+
+    double getPrice () const {
+        return price;
     }
 };
