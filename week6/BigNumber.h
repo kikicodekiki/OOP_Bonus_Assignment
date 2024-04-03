33f0526c2b946f8c385cc0a2c8143d0bff88beeb
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-04-03 16:40:48 +0300

    Create BigNumber.h

---
+++ week6/BigNumber.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <iostream>
+
+class BigNumber {
+    char* myBigNum = nullptr; //BIG 4
+
+    bool checkOnlyDigits (const char* num);
+
+    void free();
+    void copyFrom();
+
+public:
+    BigNumber() = default;
+    explicit BigNumber(const char* newNum);
+    BigNumber(long long int newNum);
+
+    BigNumber& operator= (const BigNumber& other);
+
+
+    bool operator == (const BigNumber& other) const;
+    bool operator != (const BigNumber& other) const;
+    bool operator < (const BigNumber& other) const;
+    bool operator > (const BigNumber& other) const;
+
+    BigNumber& operator += (const BigNumber& other);
+    BigNumber operator + (const BigNumber& other) const;
+    BigNumber& operator -= (const BigNumber& other);
+    BigNumber operator - (const BigNumber& other) const;
+    BigNumber& operator *= (const BigNumber& other);
+    BigNumber operator * (const BigNumber& other) const;
+
+    BigNumber& operator ++ ();
+    BigNumber operator ++ (int);
+
+    BigNumber& operator -- ();
+    BigNumber operator -- (int);
+
+    BigNumber& operator - ();
+
+    BigNumber& operator /= (const BigNumber& other);
+    BigNumber operator / (const BigNumber& other) const;
+    BigNumber& operator %= (const BigNumber& other);
+    BigNumber operator % (const BigNumber& other) const;
+
+    ~BigNumber();
+};
