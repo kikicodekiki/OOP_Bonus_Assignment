c558336971051dc10d00103b660e5d965c1b46ff
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-04-03 18:10:29 +0300

    Update myMultiSet.cpp

--- practiceTest1/MultiSet/myMultiSet.cpp
+++ practiceTest1/MultiSet/myMultiSet.cpp
@@ -51,6 +51,32 @@ MultiSetLessThanThree::~MultiSetLessThanThree() {
 
 bool MultiSetLessThanThree::add (unsigned n) {
 
+    unsigned bucketIndex = getBucketIndex(n);
+    unsigned bitIndex = getBitIndex(n);
+
+    uint8_t& currBucket = buckets[bucketIndex]; //za da mozhem direktno da manipulirame
+
+    uint8_t mask = 1 << (bitIndex*2); //zashtoto dve kkletki predstavqt edna cifra
+
+    unsigned occurrences = getCountOccurrences(n);
+
+    switch (occurrences) {
+        case 3:
+        case 1: {
+            currBucket &= ~mask; // Clear the bit
+            return true;
+        }
+        case 2: {
+            currBucket |= mask; // Set the bit
+            mask <<= 1; // Move to the next bit
+            currBucket &= ~mask; // Clear the next bit
+            return true;
+        }
+        default:
+            return false;
+    }
+
+
 }
 
 unsigned MultiSetLessThanThree::getBucketIndex(unsigned num) const {
